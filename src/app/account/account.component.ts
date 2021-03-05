85e4b13ce8deac1febb538dccc0d417ebc3a24bb
Phil Walker <phillip-michael.walker@skybettingandgaming.com>
2021-03-05 15:24:20 +0000

    #20 - Remove unused imports

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { DataService } from '../shared/data.service';
 import { IUser } from '../models/interface';
 
