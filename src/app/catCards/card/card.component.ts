c5bd17937afe437ffe388fb108a8f1de5ffc2ef1
Callum Friend <callum.friend@skybettingandgaming.com>
2021-03-11 18:25:00 +0000

    adjusted food type entry

--- src/app/catCards/card/card.component.ts
+++ src/app/catCards/card/card.component.ts
@@ -25,13 +25,15 @@ export class CardComponent implements OnInit {
   }
   catslist: ICats[] = [];
 
-  doFeedCat(catId: string, feed?: IFeed) {
-    if (!feed) {
-      var date = new Date();
-      var feed = {} as IFeed;
-      feed.time = date.toJSON();
-      feed.foodType = 'Basic';
+  doFeedCat(catId: string, type: string): void {
+    var date = new Date();
+    var feed = {} as IFeed;
+    feed.time = date.toJSON();
+    if (type === '') {
+      type = 'Basic';
     }
+    feed.foodType = type;
+    console.log(feed);
     this.dataService.feedCat(catId, feed).subscribe((data) => {});
     this.cardview.getTheCats();
   }
